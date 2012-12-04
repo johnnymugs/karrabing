c9f8f01bbcbab18da09079ffa126786cbd63d725
Johnny Mukai <johnny.mukai@gmail.com>
2012-12-03 21:20:58 -0500

    Rails greenfield

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Karrabing::Application.config.secret_token = '4440ce2879c5f6fdec70c0e44f5d74dedc96aadfb640dfe2eea72e7cb3fcbca2684389385a48a94e636f697953ae3fd8ca679e07a6feed86c264b9511d3c7567'
